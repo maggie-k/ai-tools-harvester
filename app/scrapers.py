cbaedb6f3b7879fb7e4260c8d364348d70f889d9
maggie-k <k.maggiewamboi@gmail.com>
2025-12-22 10:54:29 +0300

    Update scrapers.py

--- app/scrapers.py
+++ app/scrapers.py
@@ -1,9 +1,4 @@
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
-#from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
-import pandas as pd
-import time
-import traceback
-import csv
 
 def scrape_tools():
 
@@ -11,8 +6,7 @@ def scrape_tools():
 		print("\nScraping in progress...")
 
 		with sync_playwright() as p:	
-			browser= p.chromium.launch(headless=True) 
-			# the browser variable, only accessible inside the with block
+			browser= p.chromium.launch(headless=True)			
 			context= browser.new_context()
 			page= browser.new_page()
 
@@ -20,9 +14,8 @@ def scrape_tools():
 			page.set_default_navigation_timeout(90000)
 
 			print("\nConnecting to website...")
-			page.goto("https://aitoolsdirectory.com/")		
-
-			# p waits automatically for selector to appear
+			page.goto("https://aitoolsdirectory.com/")
+			
 			print("\nWaiting for page to load...")
 			page.wait_for_load_state("networkidle")	
 
@@ -36,8 +29,7 @@ def scrape_tools():
 				print(f"\n{'='*50}")
 				print(f"\n--- Scraping Page {page_num} ---")
 				print(f"\n{'='*50}")
-
-				# grabs parent container first
+				
 				cards= page.query_selector_all(".sv-tile__info")
 
 				skip = (
@@ -48,42 +40,31 @@ def scrape_tools():
 
 				print("\nExtracting elements...")
 
-				for card in cards:
-					# finds elements within each card
+				for card in cards:					
 					name_el= card.query_selector("h3")
 					cat_el= card.query_selector("div.sv-badge.sv-badge__1.sv-badge__1__1.clickable")
 					desc_el= card.query_selector(" div p")
-					link_el= card.query_selector("a")
-					
-					#if link_el== "/tool/d-id": # this got me stuck in one process!!
-						#continue
+					link_el= card.query_selector("a")				
 
 					if link_el:
 						href= link_el.get_attribute("href")
 						print(f"\nVisiting: {href}")
-
-						# opens new tab/page
+						
 						detail_page= context.new_page()
 						detail_page.set_default_timeout(90000)
-						detail_page.goto(f"https://aitoolsdirectory.com{href}", timeout=90000, wait_until="domcontentloaded") #, timeout=90000, wait_until="domcontentloaded" cant use this without wait below
-						#detail_page.wait_for_load_state("networkidle")
-						#time.sleep(3)
-
-						# scrapes the detail page
+						detail_page.goto(f"https://aitoolsdirectory.com{href}", timeout=90000, wait_until="domcontentloaded")
+						
 
 						try:
-							link_el1= detail_page.wait_for_selector("div.sv-product-page__string a", timeout=5000) # wait_for_selector not ideal it seems!
-							actual_link= link_el1.get_attribute("href").strip() if link_el1 else "N/A" # this doesnt work with wait..
-
-							#print(type(actual_link))
+							link_el1= detail_page.wait_for_selector("div.sv-product-page__string a", timeout=5000) 
+							actual_link= link_el1.get_attribute("href").strip() if link_el1 else "N/A" 							
 
 						except (PlaywrightTimeoutError, TimeoutError):
 							print("⚠️ Website link not found on page")
 							actual_link= f"https://aitoolsdirectory.com{href}".strip()
 
 						print(f"Actual site link: {actual_link}")
-
-						# closes detail page and continues
+						
 						detail_page.close()
 						page.wait_for_load_state("networkidle")
 						time.sleep(1)
@@ -105,14 +86,12 @@ def scrape_tools():
 						"website": actual_link
 						})
 				
-
-				# pagination
 				next_page_num= page_num+1
 				next_link= page.query_selector(f'a[href="/?page={next_page_num}"]')
 
 				if next_link and next_link.is_visible():
 					print(f"\n-> Moving to page {next_page_num}...")
-					next_link.click() # rep page.goto()
+					next_link.click()
 					page.wait_for_load_state("networkidle")
 					time.sleep(1)
 					page_num+=1
@@ -127,25 +106,13 @@ def scrape_tools():
 			print(f"\n{'='*50}")	
 			print("\nScraping completed successfully!")
 			print(f"Total items collected: {len(data)}")
-			print(f"Total pages scraped: {page_num}")
-
-			#print(df.head())
-				## status messages
-				# with sync_playwright() context manager auto-closes everything when it exists
+			print(f"Total pages scraped: {page_num}")			
 
 	except Exception as e:
 		print(f"Error: {e}")	
 		traceback.print_exc()
 
 	return df
-'''
-def save_to_csv(data, filename="scraped_data.csv"):
-	with open(filename, "w", newline="", encoding="utf-8") as f:
-		writer= csv.DictWriter(f, fieldnames=["name", "category", "description", "website"])
-		writer.writeheader()
-		writer.writerows(data)
-
-	print(f"\nSaved {len(data)} tools to {filename}")'''
 
 
 if __name__== "__main__":
@@ -153,11 +120,3 @@ if __name__== "__main__":
 	#df= scrape_tools()
 	#df.to_csv("scraped_data1.csv", index=False)
 	print("\n Work Done!")
-
-
-# what makes scraping faster? best appproach, libraries...resources
-# what i learned linkedin post... handling missing input...
-# smartly gamble on where to use 'networkidle' vd 'dom', query_select vs wait_for_query
-# ... state vs stateless, handling missing data on the go to reduce rework...
-#... quick cleanups like trailing andpreceding spaces, this one tiny .strip command was the diff between getting my row inputs vs blanks..
-#... repetitive errors etc
